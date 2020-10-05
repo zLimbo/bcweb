2b138f48eda086fe1d3a6fa3991616e98e1facaa
zLimbo <z_limbo@foxmail.com>
2020-10-05 10:44:58 +0800

    first

---
+++ src/main/java/com/zlimbo/bcweb/BcwebApplication.java
@@ -0,0 +1,13 @@
+package com.zlimbo.bcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcwebApplication.class, args);
+    }
+
+}
